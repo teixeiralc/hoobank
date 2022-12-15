6ac03c4cfa3d813460b93d994e5906f6ba2d8f5a
Lucas Teixeira <lucascteixeira0@gmail.com>
2022-12-15 09:18:10 -0300

    build

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -5,7 +5,7 @@ import Button from './Button';
 const CTA = () => {
   return (
     <section
-      className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} md:flex-row flex-col bg-black-gradient-2 rounded-3xl box-shadow lg:gap-0 gap-5`}
+      className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} md:flex-row flex-col bg-black-gradient-2 rounded-3xl box-shadow lg:gap-0 gap-5 mt-20`}
     >
       <div className="flex-1 flex flex-col gap-5">
         <h2 className={styles.heading2}>Let’s try our service now!</h2>
